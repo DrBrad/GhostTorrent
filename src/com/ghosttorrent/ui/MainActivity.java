ab8b2e603f5fae3f0c15b94d660851321e84d2dd
DrBrad <brad@bradeagle.com>
2024-03-06 19:41:38 -0700

    UI Changes

--- src/com/ghosttorrent/ui/MainActivity.java
+++ src/com/ghosttorrent/ui/MainActivity.java
@@ -84,6 +84,21 @@ public class MainActivity extends Activity {
         return pane;
     }
 
+    public String humanReadableByteCountBin(long bytes){
+        long absB = bytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(bytes);
+        if(absB < 1024){
+            return bytes + " B";
+        }
+        long value = absB;
+        CharacterIterator ci = new StringCharacterIterator("KMGTPE");
+        for(int i = 40; i >= 0 && absB > 0xfffccccccccccccL >> i; i -= 10){
+            value >>= 10;
+            ci.next();
+        }
+        value *= Long.signum(bytes);
+        return String.format("%.1f %cB", value / 1024.0, ci.current());
+    }
+
     public class CellRenderer extends DefaultListCellRenderer {
 
         @Override
@@ -91,51 +106,58 @@ public class MainActivity extends Activity {
             Torrent torrent = (Torrent) value;
 
             JPanel pane = new JPanel();
-            pane.setLayout(new FlowLayout(FlowLayout.LEFT));
+            pane.setLayout(new GridBagLayout());//new FlowLayout(FlowLayout.LEFT));
             pane.setBorder(new MatteBorder(0, 0, 1, 0, Color.decode("#cccccc")));
 
 
             //IMAGE - 60 x 60?
 
             JPanel content = new JPanel();
-            content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
+            //content.setBackground(Color.black);
+            content.setOpaque(true);
+            content.setLayout(new GridBagLayout());
+
+            GridBagConstraints constraints = new GridBagConstraints();
+            //constraints.insets = new Insets(0, 80, 0, 0);
+            constraints.fill = GridBagConstraints.HORIZONTAL;
+            constraints.weightx = 1;
+            constraints.weighty = 1;
+            constraints.gridx = 0;
+            constraints.gridy = 0;
 
             JLabel title = new JLabel(torrent.getTitle());
             title.setFont(new Font(Font.MONOSPACED, Font.BOLD, 14));
-            content.add(title);
+            content.add(title, constraints);
 
             JLabel description = new JLabel(humanReadableByteCountBin(torrent.getObtained())+" of "+humanReadableByteCountBin(torrent.getTotal())+" (0.0%)");
             description.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
-            content.add(description);
+            constraints.gridy = 1;
+            content.add(description, constraints);
 
             JProgressBar progress = new JProgressBar();
             progress.setValue(10);
             //progress.setMaximum(100);
-            content.add(progress);
+            constraints.gridy = 2;
+            content.add(progress, constraints);
 
             //PROGRESS BAR
             JLabel details = new JLabel("Downloading from "+torrent.getPeers()+" peers");
             details.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
-            content.add(details);
+            constraints.gridy = 3;
+            content.add(details, constraints);
 
-            pane.add(content);
+
+            constraints = new GridBagConstraints();
+            constraints.insets = new Insets(10, 10, 10, 10);
+            constraints.fill = GridBagConstraints.HORIZONTAL;
+            constraints.weightx = 1;
+            constraints.weighty = 1;
+            constraints.gridx = 0;
+            constraints.gridy = 0;
+
+            pane.add(content, constraints);
 
             return pane;
         }
-    }
-
-    public static String humanReadableByteCountBin(long bytes) {
-        long absB = bytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(bytes);
-        if (absB < 1024) {
-            return bytes + " B";
-        }
-        long value = absB;
-        CharacterIterator ci = new StringCharacterIterator("KMGTPE");
-        for (int i = 40; i >= 0 && absB > 0xfffccccccccccccL >> i; i -= 10) {
-            value >>= 10;
-            ci.next();
-        }
-        value *= Long.signum(bytes);
-        return String.format("%.1f %cB", value / 1024.0, ci.current());
     }
 }
