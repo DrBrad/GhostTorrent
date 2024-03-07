dd0c410fc4e78633b965d84ce182fed4d847993a
DrBrad <brad@bradeagle.com>
2024-03-07 02:04:31 -0700

    Set field instead of method

--- src/com/ghosttorrent/Main.java
+++ src/com/ghosttorrent/Main.java
@@ -3,11 +3,20 @@ package com.ghosttorrent;
 import com.ghosttorrent.ui.GhostApplication;
 import com.ghosttorrent.ui.utils.inter.Application;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args){
         Application application = new GhostApplication();
         application.launch();
+
+
+        /*
+        FileDialog dialog = new FileDialog((Frame) null);
+        dialog.setVisible(true);
+        System.out.println(dialog.getFile());
+        */
 
         //Kademlia kad = new Kademlia();
         //kad.join(6881, InetAddress.getByName("router.bittorrent.com"), 6881);
