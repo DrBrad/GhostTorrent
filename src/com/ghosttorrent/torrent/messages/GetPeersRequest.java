1c6b1b655ae9023a9ad44d597857c558ba0eba75
DrBrad <brad@bradeagle.com>
2024-03-11 13:42:14 -0600

    INFO_HASH

--- src/com/ghosttorrent/torrent/messages/GetPeersRequest.java
+++ src/com/ghosttorrent/torrent/messages/GetPeersRequest.java
@@ -8,17 +8,26 @@ import unet.kad4.messages.inter.MethodMessageBase;
 @Message(method = "get_peers", type = MessageType.REQ_MSG)
 public class GetPeersRequest extends MethodMessageBase {
 
+    private byte[] infoHash;
+
     public GetPeersRequest(byte[] tid){
         super(tid);
     }
 
     @Override
     public BencodeObject encode(){
-        return super.encode();
+        BencodeObject ben =  super.encode();
+        ben.getBencodeObject(type.innerKey()).put("info_hash", infoHash);
+        return ben;
     }
 
     @Override
     public void decode(BencodeObject ben){
         super.decode(ben);
+        if(!ben.getBencodeObject(type.innerKey()).containsKey("info_hash")){
+            System.out.println("MISSING INFO_HASH");
+        }
+
+        infoHash = ben.getBencodeObject(type.innerKey()).getBytes("info_hash");
     }
 }
