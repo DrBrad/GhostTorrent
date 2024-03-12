a46a27b4e941df305f65c2a06a1cd5ef3f45df38
DrBrad <brad@bradeagle.com>
2024-03-12 14:34:07 -0600

    GET_PEERS Debugging

--- src/com/ghosttorrent/torrent/messages/GetPeersResponse.java
+++ src/com/ghosttorrent/torrent/messages/GetPeersResponse.java
@@ -5,12 +5,19 @@ import unet.kad4.messages.inter.Message;
 import unet.kad4.messages.inter.MessageType;
 import unet.kad4.messages.inter.MethodMessageBase;
 import unet.kad4.utils.Node;
+import unet.kad4.utils.net.AddressType;
 
+import java.net.Inet4Address;
 import java.util.ArrayList;
 import java.util.List;
 
+import static unet.kad4.utils.NodeUtils.packNodes;
+import static unet.kad4.utils.NodeUtils.unpackNodes;
+
 @Message(method = "get_peers", type = MessageType.RSP_MSG)
 public class GetPeersResponse extends MethodMessageBase {
+
+    public static final int NODE_CAP = 20;
 
     private byte[] token;
     private List<String> peers;
@@ -43,6 +50,27 @@ public class GetPeersResponse extends MethodMessageBase {
     public BencodeObject encode(){
         BencodeObject ben =  super.encode();
         ben.getBencodeObject(type.innerKey()).put("token", token);
+
+        if(peers.isEmpty()){
+            if(nodes.isEmpty()){
+                return ben;
+            }
+
+            List<Node> nodes = getIPv4Nodes();
+            if(!nodes.isEmpty()){
+                ben.getBencodeObject(type.innerKey()).put("nodes", packNodes(nodes, AddressType.IPv4));
+            }
+
+            nodes = getIPv6Nodes();
+            if(!nodes.isEmpty()){
+                ben.getBencodeObject(type.innerKey()).put("nodes6", packNodes(nodes, AddressType.IPv6));
+            }
+
+            return ben;
+        }
+
+        //WHAT ABOUT PEERS...?
+
         return ben;
     }
 
@@ -50,13 +78,30 @@ public class GetPeersResponse extends MethodMessageBase {
     public void decode(BencodeObject ben){
         super.decode(ben);
 
-        System.out.println(ben);
         if(!ben.getBencodeObject(type.innerKey()).containsKey("token")){
-            System.out.println("MISSING TOKEN");
+            //System.out.println("MISSING TOKEN");
             return;
         }
 
         token = ben.getBencodeObject(type.innerKey()).getBytes("token");
+
+        if(!ben.containsKey("values")){
+            if(!ben.getBencodeObject(type.innerKey()).containsKey("nodes") &&
+                    !ben.getBencodeObject(type.innerKey()).containsKey("nodes6")){
+                //throw new MessageException("Response to "+FIND_NODE+" did not contain 'node' or 'node6'", ErrorMessage.ErrorType.PROTOCOL);
+            }
+
+            if(ben.getBencodeObject(type.innerKey()).containsKey("nodes")){
+                nodes.addAll(unpackNodes(ben.getBencodeObject(type.innerKey()).getBytes("nodes"), AddressType.IPv4));
+            }
+
+            if(ben.getBencodeObject(type.innerKey()).containsKey("nodes6")){
+                nodes.addAll(unpackNodes(ben.getBencodeObject(type.innerKey()).getBytes("nodes6"), AddressType.IPv6));
+            }
+            return;
+        }
+
+        System.out.println(ben);
     }
 
     public void setToken(byte[] token){
@@ -75,7 +120,58 @@ public class GetPeersResponse extends MethodMessageBase {
         return peers;
     }
 
-    public List<Node> getNodes(){
+
+    public void addNode(Node node){
+        nodes.add(node);
+    }
+
+    public Node getNode(int i){
+        return nodes.get(i);
+    }
+
+    public void removeNode(Node node){
+        nodes.remove(node);
+    }
+
+    public boolean containsNode(Node node){
+        return nodes.contains(node);
+    }
+
+    public boolean hasNodes(){
+        return !nodes.isEmpty();
+    }
+
+    public void addNodes(List<Node> nodes){
+        if(nodes.size()+this.nodes.size() > NODE_CAP){
+            throw new IllegalArgumentException("Adding nodes would exceed Node Cap of "+NODE_CAP);
+        }
+
+        this.nodes.addAll(nodes);
+    }
+
+    public List<Node> getAllNodes(){
         return nodes;
+    }
+
+    public List<Node> getIPv4Nodes(){
+        List<Node> r = new ArrayList<>();
+
+        for(Node node : nodes){
+            if(node.getHostAddress() instanceof Inet4Address){
+                r.add(node);
+            }
+        }
+        return r;
+    }
+
+    public List<Node> getIPv6Nodes(){
+        List<Node> r = new ArrayList<>();
+
+        for(Node node : nodes){
+            if(node.getHostAddress() instanceof Inet4Address){
+                r.add(node);
+            }
+        }
+        return r;
     }
 }
--- src/com/ghosttorrent/torrent/trackers/DHTTracker.java
+++ src/com/ghosttorrent/torrent/trackers/DHTTracker.java
@@ -2,6 +2,7 @@ package com.ghosttorrent.torrent.trackers;
 
 import com.ghosttorrent.torrent.messages.AnnouncePeerRequest;
 import com.ghosttorrent.torrent.messages.GetPeersRequest;
+import com.ghosttorrent.torrent.messages.GetPeersResponse;
 import unet.kad4.routing.kb.KBucket;
 import unet.kad4.rpc.events.ResponseEvent;
 import unet.kad4.rpc.events.inter.ResponseCallback;
@@ -15,21 +16,38 @@ import static com.ghosttorrent.Main.kad;
 
 public class DHTTracker {
 
+    private byte[] infoHash;
+
+    //WHY IS THIS CAUSING MULTIPLE RESTARTS...
+    //WHY IS THE SERVER UNREACHABLE...
+
     public DHTTracker(byte[] infoHash){
-        GetPeersRequest request = new GetPeersRequest();
+        this.infoHash = infoHash;
 
         List<Node> nodes = kad.getRoutingTable().findClosest(new UID(infoHash), KBucket.MAX_BUCKET_SIZE);
+        getPeers(nodes, 0);
 
+
+        //THEN ANNOUNCE...
+    }
+
+    public void getPeers(List<Node> nodes, int attempts){
         for(Node node : nodes){
+            GetPeersRequest request = new GetPeersRequest();
             request.setDestination(node.getAddress());
             request.setInfoHash(infoHash);
 
             try{
-
                 kad.getServer().send(request, new ResponseCallback(){
                     @Override
                     public void onResponse(ResponseEvent event){
-                        System.out.println("RESPONSE - GET_PEERS");
+                        GetPeersResponse response = (GetPeersResponse) event.getMessage();
+                        if(response.hasNodes()){
+                            if(attempts > 4){
+                                return;
+                            }
+                            getPeers(response.getAllNodes(), attempts+1);
+                        }
                     }
                 });
 
@@ -37,8 +55,5 @@ public class DHTTracker {
                 e.printStackTrace();
             }
         }
-
-
-        //THEN ANNOUNCE...
     }
 }
