d38ae77e8c4583f16a3a9ec94c20bbbe9c803f9e
DrBrad <brad@bradeagle.com>
2024-03-12 11:52:01 -0600

    GET_PEER & ANNOUNCE

--- src/com/ghosttorrent/Main.java
+++ src/com/ghosttorrent/Main.java
@@ -64,15 +64,15 @@ public class Main {
         kad.registerMessage(SampleHashRequest.class);
         kad.registerMessage(SampleHashResponse.class);
 
-        //kad.join(6881, InetAddress.getByName("router.bittorrent.com"), 6881);
-
         /*
+        kad.join(6881, InetAddress.getByName("router.bittorrent.com"), 6881);
+
         while(true){
             Thread.sleep(10000);
             System.out.println(kad.getRoutingTable().getDerivedUID()+"  "+kad.getRoutingTable().getConsensusExternalAddress().getHostAddress());
         }
-        */
 
+        /*
         String magnet = "magnet:?xt=urn:btih:4f836d43e43e8682ff6d2e197e32d7e4f4fb6af4&dn=Ubuntu+Linux+20.04+LTS+Desktop+64-bit&tr=http%3A%2F%2Ftorrent.ubuntu.com%3A6969%2Fannounce";
         new Magnet(magnet);
 
@@ -80,6 +80,7 @@ public class Main {
 
         magnet = "magnet:?xt=urn:btih:1061709FB638C2CE8728B185846EC5974BEE370F&dn=Wish+%282023%29+%5B1080p%5D+%5BYTS.MX%5D&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.tracker.cl%3A1337%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Fipv4.tracker.harry.lu%3A80%2Fannounce&tr=https%3A%2F%2Fopentracker.i2p.rocks%3A443%2Fannounce";
         new Magnet(magnet);
+        */
 
     }
 }
