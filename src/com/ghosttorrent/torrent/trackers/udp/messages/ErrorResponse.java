2f00955ead1b216fe475fba8df692a3c658479ae
DrBrad <brad@bradeagle.com>
2024-03-16 14:15:26 -0600

    Error handling

--- src/com/ghosttorrent/torrent/trackers/udp/messages/ErrorResponse.java
+++ src/com/ghosttorrent/torrent/trackers/udp/messages/ErrorResponse.java
@@ -14,7 +14,19 @@ public class ErrorResponse extends MessageBase {
 
     @Override
     public byte[] encode(){
-        return new byte[0];
+        byte[] message = this.message.getBytes();
+        byte[] buf = new byte[message.length+8];
+
+        buf[0] = ((byte) action.getCode());
+        buf[1] = ((byte) (action.getCode() >> 8));
+        buf[2] = ((byte) (action.getCode() >> 16));
+        buf[3] = ((byte) (action.getCode() >> 24));
+
+        System.arraycopy(tid, 0, buf, 4, tid.length);
+
+        System.arraycopy(message, 0, buf, 8, message.length);
+
+        return buf;
     }
 
     @Override
